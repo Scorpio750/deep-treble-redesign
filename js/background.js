d6b8c0662f86b051a062df0ae1b51798063dfe28
Eric Cuiffo <ecuiffo@gmail.com>
2014-11-27 11:20:04 -0500

    Get rid of all this php nonsense

---
+++ js/background.js
@@ -0,0 +1,7 @@
+var prepareBG = function() {        
+  var fill = document.getElementById('fill');
+  var windowOffset = $(window).scrollTop();
+  var opac = Math.min(windowOffset/500, 1);
+  $('#fill').css({opacity: opac});
+}
+$(document).scroll(prepareBG);
--- ready.php
+++
@@ -1,9 +0,0 @@
-<script>
-  var prepareBG = function() {        
-    var fill = document.getElementById('fill');
-    var windowOffset = $(window).scrollTop();
-    var opac = Math.min(windowOffset/500, 1);
-    $('#fill').css({opacity: opac});
-  }
-  $(document).scroll(prepareBG);
-</script>
