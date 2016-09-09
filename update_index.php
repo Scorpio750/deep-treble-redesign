ae65ccc105355fd4dec3121d1a5cc0c241d0ab4a
Patrick Wu <scorpio7500@gmail.com>
2016-09-09 18:00:21 -0400

    intial commit of redesign, route to index page

--- update_index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  if ($_POST["password"] == "200percentyeah!") {
-    file_put_contents('data/homepage.html', $_POST["data"]);
-    echo "Accepted Changes.";
-  } else {
-    echo "Incorrect Password!";
-  }
-?>
