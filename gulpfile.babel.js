a72b4f0a0b52dfaa948df1fe5fcfc86d4666bf54
Patrick Wu <scorpio7500@gmail.com>
2016-09-23 01:21:03 -0400

    removed task dependencies in nodemon

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,47 +1,56 @@
 import gulp from 'gulp'
 import nodemon from 'gulp-nodemon'
+import notify from 'gulp-notify'
 import debug from 'gulp-debug'
 import webpack from 'webpack-stream'
 import wpConfig from './config/webpack.config.js'
 import sass from 'gulp-sass'
-import livereload	from 'gulp-livereload'
+import livereload from 'gulp-livereload'
 
 const config = {
-	sassPath: './public/src/scss/*.scss',
-	cssPath: './public/dist/css/',
-	jsPath: './public/src/js/*.js',
-	bundledPath: './public/dist/js/',
-	htmlPath: './public/dist/views/'
+    sassPath: './public/src/scss/*.scss',
+    cssPath: './public/dist/css/',
+    jsPath: './public/src/js/*.js',
+    bundledPath: './public/dist/js/',
+    htmlPath: './public/dist/views/'
 }
 
 gulp.task('webpack', () => {
-	return gulp.src(config.jsPath)
-	.pipe(webpack(wpConfig))
-	.pipe(gulp.dest(config.bundledPath))
-	.pipe(livereload())
+    return gulp.src(config.jsPath)
+        .pipe(webpack(wpConfig))
+        .pipe(gulp.dest(config.bundledPath))
+        .pipe(livereload())
 })
 
 gulp.task('sass', () => {
-	return gulp.src(config.sassPath)
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest(config.cssPath))
-	.pipe(livereload())
+    return gulp.src(config.sassPath)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(config.cssPath))
+        .pipe(livereload())
 })
 
 gulp.task('server', ['webpack', 'sass'], (cb) => {
-	livereload.listen()
-	nodemon({
-		script: 'server.js',
-	tasks: ['webpack', 'sass'],
-	ext: 'js html'
-	})
-.on('restart', () => {
-	livereload.changed('server.js')
-	gulp.src('server.js')
-	.pipe(notify('Restarting server...'))
-		})
+    let called = false
+    livereload.listen()
+    nodemon({
+            script: 'server.js',
+            ext: 'js html'
+        })
+        .on('start', () => {
+            if (!called) {
+                cb()
+                called = true
+            }
+        })
+        .on('restart', () => {
+            setTimeout(() => {
+                livereload.changed('server.js')
+                gulp.src('server.js')
+					.pipe(notify('Restarting server...'))
+            }, 1000)
+        })
 
-	gulp.watch(config.sassPath, ['sass'])
-	gulp.watch(config.jsPath, ['webpack'])
-	})
+    gulp.watch(config.sassPath, ['sass'])
+    gulp.watch(config.jsPath, ['webpack'])
+})
 gulp.task('default', ['server'])
--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = process.env.NODE_ENV || 3000
 
-app.get('/', function(req, res) {
-	res.sendFile(process.cwd() + '/public/dist/views/index.html');
-});
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/public/dist/views/index.html')
+})
+
+app.get('css/style.css', (req, res) => {
+	console.log('hitting styles?')
+})
 
 app.listen(PORT, function() {
-	console.log('Listening on port', PORT)
+    console.log('Listening on port', PORT)
 })
