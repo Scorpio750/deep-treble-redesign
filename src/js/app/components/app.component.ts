98e137c7852302d5eda86976e771371497568841
Patrick Wu <scorpio7500@gmail.com>
2016-09-29 04:06:24 -0400

    reorganized dir structure

---
+++ src/js/app/components/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'app',
+	template: "<h1>Welcome to Deep Treble's webpage!</h1>"
+})
+
+export class MyApp {}
---
+++ src/js/app/index.ts
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { MyAppModule } from './modules/app.module';
+
+platformBrowserDynamic().bootstrapModule(MyAppModule)
