8bd961308d00f6e223516847af3994ebdfca4971
Patrick Wu <scorpio7500@gmail.com>
2016-09-30 05:25:33 -0400

    changed default port to listen to in process.env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express()
-const PORT = process.env.NODE_ENV // || 3000
+const PORT = process.env.$PORT || 3000
 
 // bless this function
 app.use(express.static('public/views'))
