4d76942d0c9d6ab1c440f0637251fb1314eeabcb
Patrick Wu <scorpio7500@gmail.com>
2016-10-03 06:04:32 -0400

    testing adding animations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core'
+import { Component, HostBinding } from '@angular/core'
+import { trigger, transition, animate, style, state } from '@angular/core'
 
 @Component({
-	selector: 'home',
-	templateUrl: './home.component.html',
-	styleUrls: ['./home.component.scss']
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    animations: [
+        trigger('routeAnimation', [
+            state('*',
+                style({
+                    opacity: 1
+                })
+            ),
+            transition('void => *', [
+                style({
+                    opacity: 0
+                }),
+                animate('1.5s ease-in-out')
+            ]),
+            transition('* => void', [
+                animate('1.5s ease-out', style({
+                    opacity: 0
+                }))
+            ])
+        ])
+    ]
 })
 export class HomeComponent {
-	greeting = {
-		header: "Welcome to Deep Treble's Webpage",
-		body: "Please excuse our appearance as we build our new website!"
-	}
+    @HostBinding('@routeAnimation') get routeAnimation() {
+        return true
+    }
+    greeting = {
+        header: "Welcome to Deep Treble's Webpage",
+        body: "Please excuse our appearance as we build our new website!"
+    }
 }
--- src/app/components/members/members.component.js
+++ src/app/components/members/members.component.js
@@ -9,10 +9,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 const core_1 = require('@angular/core');
+const core_2 = require('@angular/core');
 const members_service_1 = require('../../services/members.service');
 let MembersComponent = class MembersComponent {
     constructor(membersService) {
         this.membersService = membersService;
+    }
+    get routeAnimation() {
+        return true;
     }
     ngOnInit() {
         this.getMembers();
@@ -21,15 +25,34 @@ let MembersComponent = class MembersComponent {
         this.membersService.getMembers()
             .then((members) => this.members = members);
     }
-    onSelect(member) {
-        this.selectedMember = member;
-    }
 };
+__decorate([
+    core_1.HostBinding('@routeAnimation'), 
+    __metadata('design:type', Object)
+], MembersComponent.prototype, "routeAnimation", null);
 MembersComponent = __decorate([
     core_1.Component({
-        selector: '<members>',
+        selector: 'members',
         templateUrl: './members.component.html',
-        styleUrls: ['./members.component.scss']
+        styleUrls: ['./members.component.scss'],
+        animations: [
+            core_2.trigger('routeAnimation', [
+                core_2.state('*', core_2.style({
+                    opacity: 1
+                })),
+                core_2.transition('void => *', [
+                    core_2.style({
+                        opacity: 0
+                    }),
+                    core_2.animate('1.5s ease-in-out')
+                ]),
+                core_2.transition('* => void', [
+                    core_2.animate('1.5s ease-out', core_2.style({
+                        opacity: 0
+                    }))
+                ])
+            ])
+        ]
     }), 
     __metadata('design:paramtypes', [members_service_1.MembersService])
 ], MembersComponent);
