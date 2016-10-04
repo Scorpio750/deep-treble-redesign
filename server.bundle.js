9e93f70c00515991bb0e1432bff3973846ce7c68
Patrick Wu <scorpio7500@gmail.com>
2016-10-04 07:15:12 -0400

    added tracking for private ip addr

--- server.bundle.js
+++ server.bundle.js
@@ -50,17 +50,26 @@
 
 	var _express2 = _interopRequireDefault(_express);
 
+	var _dns = __webpack_require__(2);
+
+	var _dns2 = _interopRequireDefault(_dns);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var app = (0, _express2.default)();
-	var PORT = process.env.$PORT || 3000;
+	var PORT = process.env.NODE_ENV || 3000;
 
 	// bless this function
 	app.use(_express2.default.static('public/views'));
 	app.use(_express2.default.static('public'));
 
-	app.listen(PORT, function () {
-	    console.log('Listening on port', PORT);
+	// get private ip addr
+	_dns2.default.lookup(__webpack_require__(3).hostname(), function (err, add, fam) {
+		var APP_PRIVATE_IP_ADDRESS = void 0;
+		APP_PRIVATE_IP_ADDRESS = add;
+		app.listen(PORT, function () {
+			console.log('Listening on port', PORT, 'at private address', APP_PRIVATE_IP_ADDRESS);
+		});
 	});
 
 /***/ },
@@ -68,6 +77,18 @@
 /***/ function(module, exports) {
 
 	module.exports = require("express");
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = require("dns");
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = require("os");
 
 /***/ }
 /******/ ]);
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express'
+import DNS from 'dns'
 
 const app = express()
-const PORT = process.env.$PORT || 3000
+const PORT = process.env.NODE_ENV || 3000
+
 
 // bless this function
 app.use(express.static('public/views'))
 app.use(express.static('public'))
 
-app.listen(PORT, () => {
-    console.log('Listening on port', PORT)
+// get private ip addr
+DNS.lookup(require('os').hostname(), function (err, add, fam) {
+	let APP_PRIVATE_IP_ADDRESS
+	APP_PRIVATE_IP_ADDRESS = add
+	app.listen(PORT, () => {
+		console.log('Listening on port', PORT, 'at private address', APP_PRIVATE_IP_ADDRESS)
+	})
 })
