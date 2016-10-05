beda54fa9033e276e9f4d8b6375d00a02cf8128d
Patrick Wu <scorpio7500@gmail.com>
2016-10-05 05:15:03 -0400

    enabled prod mode

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 "use strict";
 const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+const core_1 = require('@angular/core');
 const app_module_1 = require('./modules/app.module');
 const platform = platform_browser_dynamic_1.platformBrowserDynamic();
+core_1.enableProdMode();
 platform.bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=index.js.map
