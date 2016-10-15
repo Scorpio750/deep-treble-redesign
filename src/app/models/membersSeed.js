16cbf8e55353e60dfbf32cadd5f327d8bded14e6
Patrick Wu <scorpio7500@gmail.com>
2016-10-15 14:19:16 -0400

    compressed img filesizes by 90%

--- src/app/models/membersSeed.js
+++ src/app/models/membersSeed.js
@@ -5,105 +5,105 @@ exports.MEMBERS = [
         year: 2020,
         voicePart: 'Soprano',
         position: undefined,
-        headshotUrl: './assets/img/headshots/placeholder.jpg'
+        headshotUrl: './assets/img/headshots/placeholder-min.jpg'
     },
     {
         name: 'Kathryna Caparino',
         year: 2020,
         voicePart: 'Soprano',
         position: undefined,
-        headshotUrl: './assets/img/headshots/placeholder.jpg'
+        headshotUrl: './assets/img/headshots/placeholder-min.jpg'
     },
     {
         name: 'Leah Rozario',
         year: 2019,
         voicePart: 'Soprano',
         position: 'Musical Director',
-        headshotUrl: './assets/img/headshots/LeahHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/LeahHeadShot-min.jpg'
     },
     {
         name: 'Bria Romano',
         year: 2018,
         voicePart: 'Alto',
         position: undefined,
-        headshotUrl: './assets/img/headshots/BriaHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/BriaHeadShot-min.jpg'
     },
     {
         name: 'Claire Towell',
         year: 2019,
         voicePart: 'Alto',
         position: 'Webmaster',
-        headshotUrl: './assets/img/headshots/ClaireHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/ClaireHeadShot-min.jpg'
     },
     {
         name: 'Erica Lazarow',
         year: 2019,
         voicePart: 'Alto',
         position: 'Business Manager',
-        headshotUrl: './assets/img/headshots/EricaHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/EricaHeadShot-min.jpg'
     },
     {
         name: 'Grace Lee',
         year: 2019,
         voicePart: 'Alto',
         position: 'Asst. Musical Director',
-        headshotUrl: './assets/img/headshots/GraceHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/GraceHeadShot-min.jpg'
     },
     {
         name: 'Jannae Johnson',
         year: 2019,
         voicePart: 'Alto',
         position: undefined,
-        headshotUrl: './assets/img/headshots/placeholder.jpg'
+        headshotUrl: './assets/img/headshots/placeholder-min.jpg'
     },
     {
         name: 'Duncan Sy',
         year: 2018,
         voicePart: 'Tenor',
         position: 'Treasurer',
-        headshotUrl: './assets/img/headshots/DuncanHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/DuncanHeadShot-min.jpg'
     },
     {
         name: 'Johnny Ferreira',
         year: 2018,
         voicePart: 'Tenor',
         position: undefined,
-        headshotUrl: './assets/img/headshots/placeholder.jpg'
+        headshotUrl: './assets/img/headshots/placeholder-min.jpg'
     },
     {
         name: 'Kenny Bambridge',
         year: 2018,
         voicePart: 'Tenor',
         position: undefined,
-        headshotUrl: './assets/img/headshots/KennyHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/KennyHeadShot-min.jpg'
     },
     {
         name: 'Yoku Crentsil',
         year: 2018,
         voicePart: 'Tenor',
         position: 'Social Affairs Manager',
-        headshotUrl: './assets/img/headshots/YokuHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/YokuHeadShot-min.jpg'
     },
     {
         name: 'Joe Haverlock',
         year: 2019,
         voicePart: 'Bass',
         position: undefined,
-        headshotUrl: './assets/img/headshots/placeholder.jpg'
+        headshotUrl: './assets/img/headshots/placeholder-min.jpg'
     },
     {
         name: 'Marquis Reece',
         year: 2017,
         voicePart: 'Bass',
         position: 'Historian',
-        headshotUrl: './assets/img/headshots/MarquisHeadShot.jpg'
+        headshotUrl: './assets/img/headshots/MarquisHeadShot-min.jpg'
     },
     {
         name: 'Mason Satnick',
         year: 2020,
         voicePart: 'Bass',
         position: undefined,
-        headshotUrl: './assets/img/headshots/placeholder.jpg'
+        headshotUrl: './assets/img/headshots/placeholder-min.jpg'
     }
 ];
 //# sourceMappingURL=membersSeed.js.map
