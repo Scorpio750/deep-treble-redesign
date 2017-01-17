d4ecb55b1633c93401a7bb12b908b2067a6df23f
Patrick Wu <scorpio7500@gmail.com>
2017-01-17 01:36:56 -0500

    rebuilt everything for prod

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import DNS from 'dns'
 
 const app = express()
-const PORT = process.env.NODE_ENV || 3000
+const PORT = process.env.NODE_ENV || 8080
 
 
 // bless this function
@@ -10,7 +10,7 @@ app.use(express.static('public/views'))
 app.use(express.static('public'))
 
 // get private ip addr
-DNS.lookup(require('os').hostname(), function (err, add, fam) {
+DNS.lookup(require('os').hostname(), (err, add, fam) => {
 	let APP_PRIVATE_IP_ADDRESS
 	APP_PRIVATE_IP_ADDRESS = add
 	app.listen(PORT, () => {
